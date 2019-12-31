862691e27983f94a7dc04ca6bc555117628e48a0
Leonel Galán <leonel.galan@epifany.com>
2019-12-31 16:15:25 -0500

    Adds React Router's minimal Hello World

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This dashboard-like app is built as a proof of concept for the following technol
 
 - [x] [React](https://reactjs.org/), the easiest way: [`create-react-app`](https://github.com/facebook/create-react-app)
 - [x] Airbnb [ESLint](https://eslint.org/) configuration: [`eslint-config-airbnb`](https://www.npmjs.com/package/eslint-config-airbnb).
-
+- [x] React Router V5: [`react-router`](https://github.com/ReactTraining/react-router)
 
 ## Tools
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.jsx</code>
-        {' '}
-and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
 );
 
 export default App;
