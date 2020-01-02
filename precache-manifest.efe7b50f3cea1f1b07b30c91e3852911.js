0a32d43be70c4c51f2ff1e4f7f01368cb658a235
Leonel Galán <leonel.galan@epifany.com>
2020-01-02 11:49:48 -0500

    Updates

---
+++ precache-manifest.efe7b50f3cea1f1b07b30c91e3852911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139e5bbe1da01d6d8f64838f6abd0499",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "12806235a2f862ecf30c",
+    "url": "/dashboard/static/css/main.3c8ef5a4.chunk.css"
+  },
+  {
+    "revision": "5f62c7ceeb56db8e8ab7",
+    "url": "/dashboard/static/js/2.b06372c3.chunk.js"
+  },
+  {
+    "revision": "40acecd293a6d9db7ab7b9b074e37caa",
+    "url": "/dashboard/static/js/2.b06372c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12806235a2f862ecf30c",
+    "url": "/dashboard/static/js/main.e57ac460.chunk.js"
+  },
+  {
+    "revision": "9654e981475cd9a6fea2",
+    "url": "/dashboard/static/js/runtime-main.c59dab00.js"
+  }
+]);
