6179496299c4d6352ab0f59ead039d78b03b449c
Leonel Galán <leonel.galan@epifany.com>
2020-01-03 21:41:49 -0500

    Updates

--- precache-manifest.53ca65f4658ebfc35769cd8118a367b9.js
+++ precache-manifest.53ca65f4658ebfc35769cd8118a367b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "139e5bbe1da01d6d8f64838f6abd0499",
+    "revision": "bed30e5d7ae1ac203751e797aefca7e5",
     "url": "/dashboard/index.html"
   },
   {
-    "revision": "12806235a2f862ecf30c",
+    "revision": "31fd7625985303b984f9",
     "url": "/dashboard/static/css/main.3c8ef5a4.chunk.css"
   },
   {
-    "revision": "5f62c7ceeb56db8e8ab7",
-    "url": "/dashboard/static/js/2.b06372c3.chunk.js"
+    "revision": "272176f6a8c30c5e373d",
+    "url": "/dashboard/static/js/2.0bcfb60d.chunk.js"
   },
   {
     "revision": "40acecd293a6d9db7ab7b9b074e37caa",
-    "url": "/dashboard/static/js/2.b06372c3.chunk.js.LICENSE"
+    "url": "/dashboard/static/js/2.0bcfb60d.chunk.js.LICENSE"
   },
   {
-    "revision": "12806235a2f862ecf30c",
-    "url": "/dashboard/static/js/main.e57ac460.chunk.js"
+    "revision": "31fd7625985303b984f9",
+    "url": "/dashboard/static/js/main.9dae7323.chunk.js"
   },
   {
     "revision": "9654e981475cd9a6fea2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.efe7b50f3cea1f1b07b30c91e3852911.js"
+  "/dashboard/precache-manifest.53ca65f4658ebfc35769cd8118a367b9.js"
 );
 
 self.addEventListener('message', (event) => {
